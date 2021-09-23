197f935613367c3e6fe93c0a30345f8122f919a9
hlee443 <55458700+hlee443@users.noreply.github.com>
2021-09-22 23:21:39 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+let readlineSync = require('readline-sync');
+let logic = require("./lab-two")
+
+logic.makeCalendar(2021);
+
+var year = readlineSync.question('What is the year? ');
+
+var month = readlineSync.question('What is the month? ');
+
+var day = readlineSync.question('What is the day? ');
+
+logic.getDayOfTheWeekForUserDate(year, month, day);
